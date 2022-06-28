cda7cad1448c88efd5175fdb7a4d3813af6376c7
Ben Darwin <bcdarwin@gmail.com>
2022-06-28 13:09:36 -0400

    py-pydpiper: add version 2.0.18

--- packages/py-pydpiper/package.py
+++ packages/py-pydpiper/package.py
@@ -15,6 +15,7 @@ class PyPydpiper(PythonPackage):
 
     maintainers = ['bcdarwin']
 
+    version('2.0.18', sha256="5fec8c79978ae25e8deb4d92163b0b2b7fdc72e9")
     version('2.0.17', sha256="9a7ffaa2581cb920c96ae6d32ab7ae91a8982468")
     version('2.0.16', sha256="5283a38f3d8ae1881f49e8fb834dccb92f81acd5")
     version('2.0.15', commit="8d3f355ef8fda7cd73723734c0d4d2cc0f9f0e32")
@@ -24,14 +25,16 @@ class PyPydpiper(PythonPackage):
     depends_on('py-networkx', type=('build', 'run'))
     depends_on('py-ordered-set', type=('build', 'run'))
     depends_on('py-configargparse', type=('build', 'run'))
-    depends_on('py-jinja2', type=('build', 'run'))
+    depends_on('py-jinja2', type=('build', 'run'), when='@2.0.16:')
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-pandas', type=('build', 'run'))
-    depends_on('py-serpent', type=('build', 'run'))
-    depends_on('py-pyro4', type=('build', 'run'))
+    depends_on('py-pyro4', type=('build', 'run'), when='@:2.0.17')
+    depends_on('py-pyro5', type=('build', 'run'), when='@2.0.18:')
+    depends_on('py-simplejson', type=('build', 'run'), when='@2.0.18:')
     depends_on('py-pyminc', type=('build', 'run'))
     depends_on('py-minc2-simple', type=('build', 'run'))
     depends_on('py-qbatch', type=('build', 'run'))
 
+    depends_on('cctools', type='run', when='@2.0.18:')
     depends_on('minc-toolkit +shared', type='run')
     depends_on('minc-stuffs', type='run')
