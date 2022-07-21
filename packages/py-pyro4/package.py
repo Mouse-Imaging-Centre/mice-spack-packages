f76b3cb43c20fe9a9e448e17071772f4be7530eb
bcdarwin <bcdarwin@gmail.com>
2022-07-21 12:11:58 -0400

    py-pyro4: add missing py-serpent dependency

--- packages/py-pyro4/package.py
+++ packages/py-pyro4/package.py
@@ -14,3 +14,5 @@ class PyPyro4(PythonPackage):
     pypi     = "Pyro4/Pyro4-4.80.tar.gz"
 
     version('4.80', sha256='46847ca703de3f483fbd0b2d22622f36eff03e6ef7ec7704d4ecaa3964cb2220')
+
+    depends_on('py-serpent', type=('build', 'run'))
