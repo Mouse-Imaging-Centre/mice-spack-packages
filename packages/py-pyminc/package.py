bbe4b6cb5f0ddf32cb5146ad1661f08db2293adb
Ben Darwin <bcdarwin@gmail.com>
2022-08-23 13:25:56 -0400

    pyminc: add version 0.57

--- packages/py-pyminc/package.py
+++ packages/py-pyminc/package.py
@@ -14,6 +14,7 @@ class PyPyminc(PythonPackage):
 
     maintainers = ['bcdarwin']
 
+    version("0.57", sha256="15f8b2a47a4cce2ef09175c2012c9d33f30250846d109b1c0838139758bdf7ba")
     version('0.56', 'c81492ff56e14b9b049970ff5eef1a7b')
     version('0.55', 'c794e8bdd25852397fee7196140ae512')
     version('0.54', '44f79dc4f09781a6c8b80b1d6a58363c')
